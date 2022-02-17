1555ea11e0510468f6b4074746a42027113fd12d
Nibras Sameeh <nibras.sameeh@chaossearch.com>
2022-02-17 20:10:01 +0530

    create object group api test cases added

---
+++ terraformprovider/examples/test/CreateObjectGroupTest.tf
@@ -0,0 +1,104 @@
+terraform {
+  required_providers {
+    chaossearch = {
+      version = "~> 0.1.1"
+      source = "chaossearch/chaossearch"
+    }
+  }
+}
+provider "chaossearch" {
+  url               = "https://ap-south-1-aeternum.chaossearch.io"
+  access_key_id     = "LCE8T6HRFGJI3ZKBGMGD"
+  secret_access_key = "r5MEYkYntYvXqRSBMK6SFLQfPw7hHRQ0v5cqlkIk"
+  region            = "ap-south-1"
+  login  {
+    user_name = "service_user@chaossearch.com"
+    password = "thisIsAnEx@mple1!"
+    parent_user_id = "be4aeb53-21d5-4902-862c-9c9a17ad6675"
+  }
+}
+
+resource "chaossearch_object_group" "my-object-group-test-1" {
+  bucket = "object-group-001"
+  source = "chaos-test-data-aps1"
+  format {
+    _type            = "CSV"
+    column_delimiter = ","
+    row_delimiter    = "\n"
+    header_row       = true
+  }
+  interval {
+    mode   = 0
+    column = 0
+  }
+  index_retention {
+    for_partition = []
+    overall       = -1
+  }
+  filter {
+    obj1 {
+      field  = "key"
+      prefix = "bluebike"
+    }
+    obj2 {
+      field = "key"
+      regex = ".*"
+    }
+  }
+  options {
+    ignore_irregular = true
+  }
+  realtime = false
+}
+
+resource "chaossearch_object_group" "my-object-group-test-2" {
+  bucket = "object-group-002"
+  source = "chaos-test-data-aps1"
+  format {
+    _type            = "CSV"
+    column_delimiter = ","
+    row_delimiter    = "\n"
+    header_row       = true
+  }
+  interval {
+    mode   = 0
+    column = 0
+  }
+  index_retention {
+    for_partition = []
+    overall       = -1
+  }
+  options {
+    ignore_irregular = true
+  }
+  realtime = false
+}
+
+resource "chaossearch_object_group" "my-object-group-test-3" {
+  bucket = "object-group-002"
+  source = "chaos-test-data-aps1"
+  format {
+    _type            = "CSV"
+    column_delimiter = ","
+    row_delimiter    = "\n"
+    header_row       = true
+  }
+  interval {
+    mode   = 0
+    column = 0
+  }
+  filter {
+    obj1 {
+      field  = "key"
+      prefix = "bluebike"
+    }
+    obj2 {
+      field = "key"
+      regex = ".*"
+    }
+  }
+  options {
+    ignore_irregular = true
+  }
+  realtime = false
+}
