551171a553ddd0ebd4f44bb54f3955af99838559
dinesh_jayasinghe <dinesh.jayasinghe@chaossearch.com>
2022-02-28 13:04:01 +0530

    CHAOS-6506: sprint 1 review finding fixes

--- terraformprovider/examples/main.tf
+++ terraformprovider/examples/main.tf
@@ -6,56 +6,22 @@ terraform {
     }
   }
 }
-*/
 
-resource "chaossearch_object_group" "my-object-group-test-1" {
-bucket = "dinesh-group-209"
-source = "chaos-test-data-aps1"
-format {
-_type = "CSV"
-column_delimiter = ","
-row_delimiter = "\n"
-header_row       = true
-}
-interval {
-mode = 0
-column = 0
-}
-index_retention {
-for_partition = []
-overall = -1
-}
-filter {
-prefix_filter {
-field = "key"
-prefix = "bluebike"
-}
-regex_filter {
-field = "key"
-regex = ".*"
-}
-}
-options {
-ignore_irregular = true
-}
-realtime = false
-}
 provider "chaossearch" {
-url = "https://ap-south-1-aeternum.chaossearch.io"
-access_key_id = "LCE8T6HRFGJI3ZKBGMGD"
-secret_access_key = "r5MEYkYntYvXqRSBMK6SFLQfPw7hHRQ0v5cqlkIk"
-region            = "ap-south-1"
-login  {
-user_name = "service_user@chaossearch.com"
-password = "thisIsAnEx@mple1!"
-parent_user_id = "be4aeb53-21d5-4902-862c-9c9a17ad6675"
-    }
-
+  url               = "https://ap-south-1-aeternum.chaossearch.io"
+  access_key_id     = "LCE8T6HRFGJI3ZKBGMGD"
+  secret_access_key = "r5MEYkYntYvXqRSBMK6SFLQfPw7hHRQ0v5cqlkIk"
+  region            = "ap-south-1"
+  login {
+    user_name      = "service_user@chaossearch.com"
+    password       = "thisIsAnEx@mple1!"
+    parent_user_id = "be4aeb53-21d5-4902-862c-9c9a17ad6675"
+  }
 }
 
 
 resource "chaossearch_view" "chaossearch-create-view" {
-bucket = "dinesh-view-201"
+  bucket = "test-view-201"
 
   case_insensitive = false
 
@@ -78,7 +44,7 @@ bucket = "dinesh-view-201"
       }
     }
   }
-  }
+}
 
 
 resource "chaossearch_object_group" "my-object-group" {
@@ -115,8 +81,4 @@ resource "chaossearch_object_group" "my-object-group" {
   realtime = false
 }
 
-# resource "chaossearch_indexing_state" "my-object-group" {
-#   object_group_name = chaossearch_object_group.my-object-group.name
-#   active = true
-# }
 
