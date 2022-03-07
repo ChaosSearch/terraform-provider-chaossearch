3a101424a9831025ce5ea15050798d96a2b4ca2e
Nibras Sameeh <nibras.sameeh@chaossearch.com>
2022-03-07 13:18:09 +0530

    default values removed from variables.tf

--- terraformprovider/examples/variables.tf
+++ terraformprovider/examples/variables.tf
@@ -1,53 +1,44 @@
 variable "user_name" {
   type = string
   description = "Username"
-#  default = "aeternum@chaossearch.com"
 }
 
 variable "password" {
   type = string
   description = "Password"
-#  default = "ffpossgjjefjefojwfpjwgpwijaofnaconaonouf3n129091e901ie01292309r8jfcnsijvnsfini1j91e09ur0932hjsaakji"
 }
 
 variable "parent_user_id" {
   type = string
   description = "Parent User Id"
-#  default = "be4aeb53-21d5-4902-862c-9c9a17ad6675"
 }
 
 variable "admin_user_name" {
   type = string
   description = "Admin Username"
-#  default = "aeternum@chaossearch.com"
 }
 
 variable "admin_password" {
   type = string
   description = "Admin Password"
-#  default = "ffpossgjjefjefojwfpjwgpwijaofnaconaonouf3n129091e901ie01292309r8jfcnsijvnsfini1j91e09ur0932hjsaakji"
 }
 
 variable "url" {
   type = string
   description = "Cluster Url"
-#  default = "https://ap-south-1-aeternum.chaossearch.io"
 }
 
 variable "access_key_id" {
   type = string
   description = "Cluster Access Key Id"
-#  default = "LCE8T6HRFGJI3ZKBGMGD"
 }
 
 variable "secret_access_key" {
   type = string
   description = "Cluster Secret Access Key"
-#  default = "r5MEYkYntYvXqRSBMK6SFLQfPw7hHRQ0v5cqlkIk"
 }
 
 variable "region" {
   type = string
   description = "Cluster Region"
-#  default = "ap-south-1"
 }
